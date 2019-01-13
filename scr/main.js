73687e21aac03f4f63337579c8108e70477d2585
Sean Pettit <s.p@mac.com>
2019-01-13 00:19:24 +0000

    let to var and re-add the rest

--- scr/main.js
+++ scr/main.js
@@ -1,17 +1,17 @@
-const titleDiv = document.querySelector("#title-div");
-const hugh = document.querySelector("#hugh");
-const damien = document.querySelector("#damien");
-const subtitle = document.querySelector("#subtitle");
-const mercs = document.querySelector("#mercs");
-const scrollArrows = document.querySelector("#scroll-arrows");
-const subTitleBar = document.querySelector("#subtitle-bar");
-const contentWrapper = document.querySelector("#content-wrapper");
-const menuDiv = document.querySelector("#menu-div");
-const menuIcon = document.querySelector("#menu-icon");
-const fleet = document.querySelector("#section-three-fleet");
-const airport = document.querySelector("#section-four-airport");
-const tours = document.querySelector("#section-six-tours");
-const contact = document.querySelector("#section-eight-contact");
+var titleDiv = document.querySelector("#title-div");
+var hugh = document.querySelector("#hugh");
+var damien = document.querySelector("#damien");
+var subtitle = document.querySelector("#subtitle");
+var mercs = document.querySelector("#mercs");
+var scrollArrows = document.querySelector("#scroll-arrows");
+var subTitleBar = document.querySelector("#subtitle-bar");
+var contentWrapper = document.querySelector("#content-wrapper");
+var menuDiv = document.querySelector("#menu-div");
+var menuIcon = document.querySelector("#menu-icon");
+var fleet = document.querySelector("#section-three-fleet");
+var airport = document.querySelector("#section-four-airport");
+var tours = document.querySelector("#section-six-tours");
+var contact = document.querySelector("#section-eight-contact");
 
 function runAnimation() {
   titleDiv.classList.add("position-one");
@@ -25,15 +25,15 @@ function runAnimation() {
 }
 
 
-let isMenuOpen = false;
+var isMenuOpen = false;
 
-// menuIcon.addEventListener("transitionend", () => {
-//   console.log("transition end");
-//   if (!isMenuOpen) {
-//     menuDiv.classList.remove("is-open");
-//     menuDiv.classList.add("is-closed");
-//   }
-// });
+menuIcon.addEventListener("transitionend", () => {
+  console.log("transition end");
+  if (!isMenuOpen) {
+    menuDiv.classList.remove("is-open");
+    menuDiv.classList.add("is-closed");
+  }
+});
 
 
 
@@ -47,48 +47,48 @@ function sendTitleToTop() {
   }
 }
 
-// function showMenu() {
-//   sendTitleToTop();
-//   menuIcon.classList.toggle("rotate-icon");
-//   if (!isMenuOpen) {
-//     contentWrapper.classList.add("slide-away");
-//     menuDiv.classList.add("is-open");
-//     menuDiv.classList.remove("is-closed");
-//     isMenuOpen = true;
-//   } else {
-//     contentWrapper.classList.remove("slide-away");
-//     isMenuOpen = false;
-//   }
-// }
+function showMenu() {
+  sendTitleToTop();
+  menuIcon.classList.toggle("rotate-icon");
+  if (!isMenuOpen) {
+    contentWrapper.classList.add("slide-away");
+    menuDiv.classList.add("is-open");
+    menuDiv.classList.remove("is-closed");
+    isMenuOpen = true;
+  } else {
+    contentWrapper.classList.remove("slide-away");
+    isMenuOpen = false;
+  }
+}
 
-// function scrollToFleet() {
-//   window.scrollTo(0, 0);
-//   fleet.scrollIntoView({
-//     block: "start"
-//   });
-//   window.scrollTo(0, window.scrollY);
-// }
-// function scrollToAirport() {
-//   window.scrollTo(0, 0);
-//   airport.scrollIntoView({
+function scrollToFleet() {
+  window.scrollTo(0, 0);
+  fleet.scrollIntoView({
+    block: "start"
+  });
+  window.scrollTo(0, window.scrollY);
+}
+function scrollToAirport() {
+  window.scrollTo(0, 0);
+  airport.scrollIntoView({
 
-//   });
-//   window.scrollTo(0, window.scrollY);
-// }
-// function scrollToTours() {
-//   window.scrollTo(0, 0);
-//   tours.scrollIntoView({
+  });
+  window.scrollTo(0, window.scrollY);
+}
+function scrollToTours() {
+  window.scrollTo(0, 0);
+  tours.scrollIntoView({
 
-//   });
-//   window.scrollTo(0, window.scrollY);
-// }
-// function scrollToContact() {
-//   window.scrollTo(0, 0);
-//   contact.scrollIntoView({
+  });
+  window.scrollTo(0, window.scrollY);
+}
+function scrollToContact() {
+  window.scrollTo(0, 0);
+  contact.scrollIntoView({
 
-//   });
-//   window.scrollTo(0, window.scrollY);
-// }
+  });
+  window.scrollTo(0, window.scrollY);
+}
 
 window.onscroll = function() {
   sendTitleToTop();
